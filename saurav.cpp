918e5aee0db60be09ddc62d22192cd47c14913c3
saurav@13 <sauravraj123498@gmail.com>
2026-03-06 21:17:08 +0530

    new codes

---
+++ saurav.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*int main() {
+    int x;
+    cin>>x;
+    if(x>=18){
+        cout<<"you are an adult"<<endl;}
+    else{
+        cout<<"you are not an adult"<<endl;
+
+    }
+   return 0;
+    }
+    
+   // ...existing code...
+int main(){
+    int  arr[3];
+    cin>>arr[0]>>arr[1]>>arr[2];
+    for(int i = 0;i<3;i++){
+        cout<<arr[i]<<"\n";
+    }
+    return 0 ;
+}
+int main(){
+    string s = "saurav";
+    int len = s.size();
+    cout<<s[len - 1];
+}
+int adition(int num1, int num2){
+    int num3=  num1 +  num2;
+    return num3;
+}
+int main(){
+    int num1, num2;
+
+}
+int doSum(int arr[],int n){
+    arr[1]= 100;
+    cout<<"changed value  at index 1 is "<<arr[1];
+
+}
+int main(){
+int n=5;
+    int arr[n];
+
+for(int i =0; i<n;i++){
+    cin>>arr[i];
+    
+}
+for (int i = 0;i<n;i++){
+    cout<<arr[i]<<" "<<endl;}
+
+    doSum(arr, n);
+
+}
+
+int main(){
+    vector<int>vec;
+    int n, value;
+     cout<<"enter the number of elements"<<endl;
+     cin>>n;
+     vec.resize(n);
+    
+    for(int i =0;i<n;i++){
+        cin>>vec[i];
+              
+        
+    }
+    cout<<"vector elements are "<<endl;
+    for(int i = 0;i<n;i++){
+        cout<<vec[i]<<"\n";
+    }
+    cout<<"vector is "<<vec[1]<<endl;
+    vector<int>::iterator it = vec.begin();
+    it= it+1;
+    cout<<"iterating 1st element in vector "<<*(it);
+}
+
+int main(){
+    int n;
+    cout<<"enter the number of stars you want to print"<<endl;
+    cin>>n;
+    for(int i =0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<"*";
+            
+        }
+        cout<<" "<<endl;
+    }
+}
+int main(){
+    int n;h
+    cout<<"enter the number u want to start from"<<endl;
+    cin>>n;
+    for(int i =1;i<=n;i++){
+    for(int j = 1;j<=i;j++){
+        cout<<i;
+    
+    }
+    cout<<" "<<endl;
+    }
+}
+    
+int main(){
+    int n;
+    cout<<"enter the number you want to start the decrement from"<<endl;
+    cin>>n;
+    for(int i =0;i<n;i++){
+        for (int j = 0; j<n-i+1;j++){
+            cout<<j; //enter j for number like 54321 4321 321 21 1
+
+        }
+        cout<<endl;
+    }
+}
