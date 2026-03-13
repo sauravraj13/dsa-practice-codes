71a5f0ec0fe736e257710416ddc64e28c28f906a
saurav@13 <sauravraj123498@gmail.com>
2026-03-13 23:09:35 +0530

    two sum leetcode

--- saurav.cpp
+++ saurav.cpp
@@ -111,7 +111,27 @@ int main(){
         }
         cout<<endl;
     }
-}  cout<<"MAIN Frame after changes is "
+}  */
+// two sum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i =0;i<n;i++){
+            for(int j = 0;j<n;j++)
+            if(i==j){
+            continue;
+        }
+        {
+                if(nums[i]+arr[j]== target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+        
+    }
+};
     
 
     
