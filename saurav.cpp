f3f43d8ee660799946a57f534d7cb1c2157cb640
saurav@13 <sauravraj123498@gmail.com>
2026-03-14 23:10:20 +0530

    best time to buy and sell stock

--- saurav.cpp
+++ saurav.cpp
@@ -134,6 +134,22 @@ public:
     }
 };
 // two sum with sorted array leetcode 167
-    
+// best time to buy and sell stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int price = 0;
+
+        for(int i = 1; i < prices.size(); i++){ // O(n)
+            if(buy > prices[i]){
+                buy = prices[i];
+            }
+            price = max(price, (prices[i] - buy));
+        }
+
+        return price;
+    }
+};
 
     
