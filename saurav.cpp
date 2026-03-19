fa2506222c0f5f212f303a60c591aa5ba1a91ece
saurav@13 <sauravraj123498@gmail.com>
2026-03-19 22:17:30 +0530

    leetcode 238

--- saurav.cpp
+++ saurav.cpp
@@ -226,5 +226,26 @@ public:
         return res;
     }
 };
+// leetcode 238 product of array itself
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>prefix(n,1);
+       vector<int>suffix(n,1);
+        vector<int>result(n);     
+        
+        for(int m=1;m<n;m++){
+            prefix[m] = prefix[m-1]*nums[m-1];            
+        }for(int j = n-2;j>=0;j--){
+            suffix[j] = suffix[j+1]*nums[j+1];
+        }for(int i =0;i<n;i++){
+        result[i] = suffix[i]*prefix[i];
+        }
+        return result;
+ 
+    }
+};
+     
 
     
