a1d84e01214177d5282dc206769e8f20fd754f15
saurav@13 <sauravraj123498@gmail.com>
2026-03-20 21:42:08 +0530

    leetcode 153

--- saurav.cpp
+++ saurav.cpp
@@ -246,6 +246,22 @@ public:
  
     }
 };
+leetcode 152 minimum in rotated sorted array ;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+         
+        int n = nums.size();
+        int mini = nums[0];
+        for(int i = 1; i<n;i++){
+            if(nums[i]<mini){
+                mini = nums[i];
+            }
+        }
+        return mini;
+        
+    }
+};
      
 
     
