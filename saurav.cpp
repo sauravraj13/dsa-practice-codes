f10d3b4d9f285bf623caa67dfda13e238b8dfa74
saurav@13 <sauravraj123498@gmail.com>
2026-03-23 21:21:17 +0530

    l-33 search in rotated sorted array

--- saurav.cpp
+++ saurav.cpp
@@ -294,7 +294,21 @@ public:
         return closest;
     }
 };
-*
+// search in rotated sorted array l- 33
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
      
 
     
