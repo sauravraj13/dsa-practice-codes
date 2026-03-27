a0d71f5e461ec1351405813c2a620cc866ef5027
saurav@13 <sauravraj123498@gmail.com>
2026-03-27 23:32:52 +0530

    leetcode 234

--- saurav.cpp
+++ saurav.cpp
@@ -400,5 +400,31 @@ public:
     }
    
 };
+// check palindrom in ll l-234
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode* temp = head;
+
+        // Step 1: push all values
+        while(temp != NULL){
+            st.push(temp->val);
+            temp = temp->next;
+        }
+
+        // Step 2: compare while traversing again
+        temp = head;
+        while(temp != NULL){
+            if(temp->val != st.top()){
+                return false;
+            }
+           st.pop();
+            temp = temp->next;
+        }
+
+        return true;
+    }
+};
 
     
