8dffc6c520ef85df617c88c5eefa1acdd938eb47
saurav@13 <sauravraj123498@gmail.com>
2026-03-27 23:34:49 +0530

    l141 linked list cycle

--- saurav.cpp
+++ saurav.cpp
@@ -426,5 +426,24 @@ public:
         return true;
     }
 };
+// leetcode 141 check linked list cycle
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+            
+        }
+        return false;
+        
+    }
+};
     
